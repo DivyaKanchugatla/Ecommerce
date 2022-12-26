2e6b2beadffe947f10dc4a0601bb596ade28b82b
ambikav98 <ambikav600@gamil.com>
2022-12-26 18:07:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import OwlDemo from './components/Footer/OwlDemo'
+import Footer from './components/Footer/Footer'
+import Newcontactus from './components/Contactus/Newcontactus'
 const App = () => {
   return (
     <>
-    
+    <OwlDemo/>
+    <Footer/>
+    <Newcontactus/>
     </>
   )
 }
