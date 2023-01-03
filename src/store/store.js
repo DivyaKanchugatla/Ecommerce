bdd1a97ff3f8c527d90b09487b63f0b9102e4723
DIVYA KANCHUGATLA <divyakanchugatla5@gmail.com>
2023-01-03 17:00:42 +0530

    update

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,21 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import reducers from './reducers/index';
 import thunk from 'redux-thunk';
 import { logger } from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
  
 
 const middleware = applyMiddleware(thunk,logger)
+const persistConfig = {
+    key: 'authType',
+    storage: storage,
+    // whitelist: ['authType']  which reducer want to store
+  };
+  const pReducer = persistReducer(persistConfig, reducers);
+  
+const store = createStore(pReducer,composeWithDevTools(middleware))
+const persistor = persistStore(store);
 
-
-const store = createStore(reducers,composeWithDevTools(middleware))
 export default store;
+export {persistor}
 
