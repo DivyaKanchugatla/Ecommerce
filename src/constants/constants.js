f5a003bed02c0c04f0ed2a0f5960a772059f445a
Prasadtulasi <sivaprasadtulasi480@gmail.com>
2023-01-03 17:00:33 +0530

    constants

---
+++ src/constants/constants.js
@@ -0,0 +1,8 @@
+export const Constants = {
+    CARTSUMMARY:'Cart Summary',
+    SUBTOTAL:'Subtotal',
+    SHIPPING:'Shipping',
+    FREE:'Free',
+    TOTAL:"Total",
+    PROCEEDTOCHECKOUT:'Proceed To Checkout'
+}
