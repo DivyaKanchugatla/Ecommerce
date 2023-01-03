e84d784fe3a284e60ee86bcc8b4c27a63e3d2d1d
Swamy <swamykumar539@gmail.com>
2023-01-03 17:09:44 +0530

    test

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,20 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import reducers from './reducers/index';
 import thunk from 'redux-thunk';
 import { logger } from 'redux-logger';
- 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'persist-key',
+    storage
+   
+  }
 
 const middleware = applyMiddleware(thunk,logger)
 
-
-const store = createStore(reducers,composeWithDevTools(middleware))
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = createStore(persistedReducer,composeWithDevTools(middleware))
+const persistor = persistStore(store);
 export default store;
+export {persistor}
 
