3d1ccd6de7be14a462149badebed8cce574eddbf
DIVYA KANCHUGATLA <divyakanchugatla5@gmail.com>
2023-01-06 22:24:21 +0530

    divya

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,10 @@
 import { combineReducers } from "redux";
-import {productReducer,selectedProductsReducer} from './ProductReducers'
+import {productReducer} from './ProductReducers'
 import cartReducer from './CartReducer'
 import { productDetailReducer } from "./ProductDetailReducer";
 
 const reducers = combineReducers({
     allProducts: productReducer,
-     product:selectedProductsReducer,
      _cardProduct: cartReducer,
      productDetail:productDetailReducer
 })
