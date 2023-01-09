d3fa16b3e5f709a6cd729ba0f126131e74d0f39a
DIVYA KANCHUGATLA <divyakanchugatla5@gmail.com>
2023-01-09 12:45:25 +0530

    divya

--- src/store/actions/ProductActions.js
+++ src/store/actions/ProductActions.js
@@ -10,34 +10,3 @@ export const FETCH_PRODUCTS = "FETCH_PRODUCTS"
     
  };
 
-//  export const getSingleProduct=(id)=>{
-//   return async (dispatch)=>{
-//     try {
-//       const response= await FakeStoreApi.get(`/products/${id}`)
-//     const singleProduct=await response.data
-//     dispatch(selectedProduct(singleProduct))
-//     } catch (error) {
-//       const errMsg=error.message
-//       dispatch(errMsg)
-//     }
-//   }
-//  }
-//    export const removeSelectedProduct = () => {
-//      return {
-//        type: REMOVE_SELECTED_PRODUCT,
-//      };
-//    };
-  
-// export const selectedProduct = (product) => {
-//     return {
-//       type: SELECTED_PRODUCT,
-//       payload: product,
-//     };
-//   };
-
-//   export const setProducts = (products) => {
-//     return{
-//         type: ActionTypes.SET_PRODUCTS,
-//         payload: products,
-//     }
-// };
--- src/store/reducers/ProductReducers.js
+++ src/store/reducers/ProductReducers.js
@@ -12,15 +12,3 @@ const initialState = {
          return state;
     }
  };
-//  export const selectedProductsReducer = (state = {}, { type, payload }) => {
-//      switch (type) {
-//        case ActionTypes.SELECTED_PRODUCT:
-//          return { ...state, ...payload };
-//        case ActionTypes.REMOVE_SELECTED_PRODUCT:
-//          return {};
-//        default:
-//          return state;
-//      }
-//    };
-  
- // import { GET, SUCCESS, ERROR } from "..Actions/ActionTypes";
