93a6f4d62df12e36b9793effeee9dde54c2698f6
Prasadtulasi <sivaprasadtulasi480@gmail.com>
2023-01-09 13:45:43 +0530

    Filter Component Updated

--- src/components/Shop/ProductComponent/ProductComponent.js
+++ src/components/Shop/ProductComponent/ProductComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+ import React from 'react'
  import { useEffect } from 'react';
  import { useSelector,useDispatch } from 'react-redux';
  import {fetchProducts} from '../../../store/actions/ProductActions'
