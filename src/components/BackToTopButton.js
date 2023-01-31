6a7684eb91f8f52e03d3ba7bd362c3bdf39b2607
DIVYA KANCHUGATLA <divyakanchugatla5@gmail.com>
2023-01-31 17:03:03 +0530

    update

--- src/components/BackToTopButton.js
+++ src/components/BackToTopButton.js
@@ -1,6 +1,5 @@
-
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const ButtonContainer = styled.span`
   position: fixed;
