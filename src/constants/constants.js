2d8e452055c23e872e6ea0a26b36dd1c58f15fdc
Prasadtulasi <sivaprasadtulasi480@gmail.com>
2023-01-31 17:03:22 +0530

    constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,9 @@
 export const Constants = {
-    OURSHOP:"OUR SHOP"
+    OURSHOP:"OUR SHOP",
+    HOME:"Home",
+    SHOP:"Shop",
+    FILTERBYPRICE:"Filter By Price",
+    FILTERBYCOLOR:"Filter By Color",
+    FILTERBYSIZE:"Filter By Size",
+    VIEWDETAIL:"View Detail"
 }
