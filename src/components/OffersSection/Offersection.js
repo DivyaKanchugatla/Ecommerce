6601d52ca02200357b9ca9d4db23fe6267c7c6b9
Swamy <swamykumar539@gmail.com>
2023-02-01 09:10:02 +0530

    changes

--- src/components/OffersSection/Offersection.js
+++ src/components/OffersSection/Offersection.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { DefaultConstants } from '../../constants/constants'
+import Button from '../UI/Button'
+
 import './Offersection.css'
 
 //done by kumar swamy
@@ -13,20 +15,20 @@ const Offersection = () => {
                         <div>
                         <img className='offer-section-girl-image-1' alt="girlpic" src='https://technext.github.io/eshopper/img/offer-1.png'/>
                         </div>
-                        <div className='pr-3  pt-3 text-center'>
-                            <h4 className='offer-section-description  fs-5 fw-normal'>{DefaultConstants.TWENTYPERCENTAGEOFFTHEALLOPDER}</h4>
-                            <h1 className='fs-4 fw-bold'>{DefaultConstants.SPRINGCOLLECTION}</h1>
-                            <button className='btn btn-sm fs-6 fst-normal shop-now-button'>{DefaultConstants.SHOPNOW}</button>
-                        </div>
+                        <div className='pr-3  pt-3 text-end'>
+                            <h4 className='offer-section-description  fs-4 fw-normal pb-2'>{DefaultConstants.TWENTYPERCENTAGEOFFTHEALLORDER}</h4>
+                            <h1 className='fs-3 fw-bold pb-2'>{DefaultConstants.SPRINGCOLLECTION}</h1>
+                           <Button/>
+                            </div>
                     </div>  
                 </div>
                 <div className='col-md-6'>
                     <div className='small-container mb-2  d-flex flex-row justify-content-between'>
-                        <div className='p-2 pt-2 justify-content-start'>
-                            <h4 className='offer-section-description fs-5 fw-normal'>{DefaultConstants.TWENTYPERCENTAGEOFFTHEALLOPDER}</h4>
-                            <h1 className='fs-4 fw-bold'>{DefaultConstants.WINTERCOLLECTION}</h1>
-                            <button className='btn btn-sm fs-6 fst-normal shop-now-button'>{DefaultConstants.SHOPNOW}</button>
-                        </div>
+                        <div className='pt-3 ml-3 justify-content-start'>
+                            <h4 className='offer-section-description fs-4 fw-normal pb-2'>{DefaultConstants.TWENTYPERCENTAGEOFFTHEALLORDER}</h4>
+                            <h1 className='fs-3 fw-bold pb-2'>{DefaultConstants.WINTERCOLLECTION}</h1>
+                            <Button/>
+                            </div>
                         <img className='offer-section-girl-image' alt="girlpic" src='https://technext.github.io/eshopper/img/offer-2.png' />
                         </div>  
                 </div> 
---
+++ src/components/UI/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { DefaultConstants } from '../../constants/constants';
+import './Button.css'
+
+const Button = () => {
+  const navigator=useNavigate()
+  
+  const goToShopHandler=()=>{
+    navigator('/shop')
+  }
+  
+  return (
+          <button className='btn btn-sm fs-6 fst-normal shop-now-button' onClick={goToShopHandler}>{DefaultConstants.SHOPNOW}</button>
+    )
+}
+
+export default Button
